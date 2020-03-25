8302d1c5a1bb93f31e9d7f19b38c412ae8da9712
osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2020-03-25 14:47:35 +0100

    my javascript task using arrary to store my name and courses

---
+++ main.js
@@ -0,0 +1,16 @@
+//my name and courses in array
+//autor: idiaghe osaigbovo
+//slack username: osayiosa
+
+let fullname = "Idiaghe Osaigbovo";
+
+let courses = ['HTML', 'CSS', 'JAVASCRIPT'];
+
+console.log(fullname); 
+console.log(courses);
+
+for(i=1; i<=200; i++){
+	if(i%2 === 0){
+		console.log(i);
+	}
+}
